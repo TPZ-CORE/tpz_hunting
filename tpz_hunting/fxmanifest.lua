86513f1286c2bab3324991f9ee11b452ccb76044
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2024-01-08 19:15:30 +0200

    Update fxmanifest.lua
    Added version parameter on fxmanifest.

--- tpz_hunting/fxmanifest.lua
+++ tpz_hunting/fxmanifest.lua
@@ -5,6 +5,8 @@ rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aw
 author 'Nosmakos'
 description 'TPZ-CORE Hunting'
 
+version '1.0.0'
+
 shared_scripts { 'config.lua', 'config_animals.lua', 'locales.lua' }
 server_scripts { 'server/*.lua' }
 client_scripts { 'client/*.lua', 'client/main.js' }
