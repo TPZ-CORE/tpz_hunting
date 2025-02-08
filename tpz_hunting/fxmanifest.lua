9cde521816f711b4369bcc3ada065c01ec7ba5ce
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-02-08 08:09:08 +0200

    Update fxmanifest.lua

--- tpz_hunting/fxmanifest.lua
+++ tpz_hunting/fxmanifest.lua
@@ -3,8 +3,7 @@ games {"rdr3"}
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
 author 'Nosmakos'
-description 'TPZ-CORE Hunting'
-
+description 'TPZ-CORE - Hunting'
 version '1.0.0'
 
 shared_scripts { 'config.lua', 'config_animals.lua', 'locales.lua' }
@@ -12,5 +11,11 @@ server_scripts { 'server/*.lua' }
 client_scripts { 'client/*.lua', 'client/main.js' }
 
 exports { 'DataViewNativeGetEventData' }
+
+dependencies {
+    'tpz_core',
+    'tpz_characters',
+    'tpz_inventory',
+}
 
 lua54 'yes'
