e5e86731a22181ac9018024dcea00246778a2430
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-09-12 15:00:27 +0300

    Update tp-client_butchers.lua

--- tpz_hunting/client/tp-client_butchers.lua
+++ tpz_hunting/client/tp-client_butchers.lua
@@ -9,72 +9,70 @@ Citizen.CreateThread(function()
     while true do
         Citizen.Wait(0)
 
-        local sleep  = true
+        local sleep        = 1000
 
-        local player = PlayerPedId()
-        local coords = GetEntityCoords(player)
-        local isDead = IsEntityDead(player)
+        local player       = PlayerPedId()
+        local coords       = GetEntityCoords(player)
+        local isPlayerDead = IsEntityDead(player)
 
-        if not isDead then
+        if isPlayerDead then 
+            goto END
+        end
 
-            for index, locationConfig in pairs(Config.Locations) do
+        for index, locationConfig in pairs(Config.Locations) do
 
-                local coordsDist  = vector3(coords.x, coords.y, coords.z)
-                local coordsStore = vector3(locationConfig.Coords.x, locationConfig.Coords.y, locationConfig.Coords.z)
-                local distance    = #(coordsDist - coordsStore)
+            local distance = #(coords - vector3(locationConfig.Coords.x, locationConfig.Coords.y, locationConfig.Coords.z))
 
-                if ( distance > Config.NPCRenderingDistance ) then
-                    
-                    if locationConfig.NPC then
-          
-                        RemoveEntityProperly(locationConfig.NPC, GetHashKey(locationConfig.NPCData.Model))
-                        Config.Locations[index].NPC = nil
-                    end
-
-                end
-
-                if distance <= Config.NPCRenderingDistance and not locationConfig.NPC and locationConfig.NPCData.Enabled then
-                    SpawnNPC(index)
-                end
-
-                if distance <= locationConfig.ActionDistance then
-                    sleep = false
-
-                    local promptGroup, promptList = GetPromptData()
-
-                    local label = CreateVarString(10, 'LITERAL_STRING', Locales['PROMPT_ACTION'])
-                    PromptSetActiveGroupThisFrame(promptGroup, label)
-
-                    for index, prompt in pairs (promptList) do
-
-                        if not Config.Prompts['STORE'].enabled and prompt.type == 'STORE' then
-                            PromptSetEnabled(prompt.prompt, 0)
-                            PromptSetVisible(prompt.prompt, 0)
-                        end
-
-                        if PromptHasHoldModeCompleted(prompt.prompt) then
-
-                            if prompt.type == 'SELL' then
-                                SellButcherAnimal()
-    
-                            elseif prompt.type == 'STORE' then
-                                Wait(500)
-                                exports.tpz_stores:OpenStoreByName('BUTCHER')
-                            end
-
-                            Wait(2000)
-                        end
-    
-                        
-                    end
-
+            if ( distance > Config.NPCRenderingDistance ) then
+                
+                if locationConfig.NPC then
+      
+                    RemoveEntityProperly(locationConfig.NPC, GetHashKey(locationConfig.NPCData.Model))
+                    Config.Locations[index].NPC = nil
                 end
 
             end
-        end
 
-        if sleep then
-            Citizen.Wait(1000)
+            if distance <= Config.NPCRenderingDistance and not locationConfig.NPC and locationConfig.NPCData.Enabled then
+                SpawnNPC(index)
+            end
+
+            if distance <= locationConfig.ActionDistance then
+                sleep = 0
+
+                local promptGroup, promptList = GetPromptData()
+
+                local label = CreateVarString(10, 'LITERAL_STRING', Locales['PROMPT_ACTION'])
+                PromptSetActiveGroupThisFrame(promptGroup, label)
+
+                for index, prompt in pairs (promptList) do
+
+                    if not Config.Prompts['STORE'].enabled and prompt.type == 'STORE' then
+                        PromptSetEnabled(prompt.prompt, 0)
+                        PromptSetVisible(prompt.prompt, 0)
+                    end
+
+                    if PromptHasHoldModeCompleted(prompt.prompt) then
+
+                        if prompt.type == 'SELL' then
+                            SellButcherAnimal()
+
+                        elseif prompt.type == 'STORE' then
+                            Wait(500)
+                            exports.tp_stores:openStoreByName('BUTCHER')
+                        end
+
+                        Wait(2000)
+                    end
+
+                    
+                end
+
+            end
+
         end
+       
+        ::END::
+        Wait(sleep)
     end
 end)
